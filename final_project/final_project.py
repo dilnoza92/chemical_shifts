eae26535cccf4cadbd40b355d5b2ae3458abdd46
Dilnoza Amirkulova <dilnoza92@gmail.com>
2016-12-14 13:58:09 -0500

    test

--- final_project/final_project.py
+++ final_project/final_project.py
@@ -68,6 +68,7 @@ def numbers(filename):
     return number_array[1:]
 
 #an array will hold the data from the output of the COLVAR file
+
 noe_results=numbers(colvar_file)         #output of the COLVAR file are actually stored here.
 
 power=1/6.0#power
@@ -80,6 +81,7 @@ def variables(filename):
     line0_strip=line0.strip()                #gets rid of the newline comment
     line0_split=line0_strip.split()          #splits the line into strings whenever there is a space
     return line0_split[3:]                   #gets rid of the first 3 variables
+
 def noe_f(results):
     '''Computes the noe by taking the array of noe and print the average noe and distance restraints
     Return: NOE intensity and NOE distance'''
@@ -110,17 +112,17 @@ def chemical_shifts(cs_file, shift_type):
     return chem_shifts
 shiftss=chemical_shifts(cs_file_name, 'nh')
 last_atom=len(shiftss)-1
-shiftss[0]=124
-shiftss[last_atom]=123
+shiftss[0]=105
+shiftss[last_atom]=135
 len(shiftss)
 for i in range(len(shiftss)):
     shiftss[i]=float(shiftss[i])
-def Total_atoms(variables_list):
+def total_atom(variables_list):
     '''takes an array of variable names and counts the total number of atoms interacting to make noes
     args: variables_list-an array of variable names for interacting atoms
     returns an array that has all the interacting atoms labeled 
     '''
-    Total_number_atoms=0                   #total number of atoms interacting
+    total_number_atoms=0                   #total number of atoms interacting
     atom_labels=[]#array that keeps labels of atoms
     for i in range(len(variables_list)):   #index for the variables argument
         word=variables_list[i]                              #reads each label in the list
@@ -135,7 +137,7 @@ def Total_atoms(variables_list):
         atom_identity.append(shiftss[k])         #  the index that keeps track of the for loop will also be added as an actual atom number later
         atom_labels.append(atom_identity)  #both the old atom value and the new atom value will be added to the array that keeps track of labels
         if (atom1_2[0]!=atom1_2_next[0]):#check the atom values whether the next atom and previous atom are the same
-            Total_number_atoms=i           #updates the total number of atoms even when the consecutive atoms differ
+            total_number_atoms=i           #updates the total number of atoms even when the consecutive atoms differ
             last_atom=[int(atom1_2[0])]    #if the if statement is true it will add the first pivot atom itself around which the all the other atoms were labeled 
             last_atom.append(shiftss[0])            #keep track of the atoms in the list
             atom_labels.append(last_atom)  #first atom and its new index are added
@@ -152,9 +154,9 @@ def converted_variables(filename):
     line0_split=line0_strip.split()        #splits the line into strings whenever there is a space
     line0_split=line0_split[3:]
     chem_shifts_var=[]
-    total_atoms=Total_atoms(old_variables)
+    total_atoms=total_atom(old_variables)
     number_atoms=len(total_atoms)
-    for atoms  in Total_atoms(old_variables):
+    for atoms  in total_atom(old_variables):
         for i in range(len(line0_split)):
     
            
@@ -169,6 +171,7 @@ def converted_variables(filename):
     for i in range(number_atoms):
         word=line0_split[i]
         line0_split[i]=word.replace('_{}_'.format(total_atoms[number_atoms-1][0]), '_{}_'.format(total_atoms[number_atoms-1][1]))
+    print line0_split[0]
     return line0_split        
 new_variables=converted_variables(colvar_file)
 variable_averagenoe=[]                     #empty array that contains the atoms that will have strong intensities and show intensities and restraints
@@ -191,7 +194,7 @@ def atoms_distances(array_of_comb_numb):
     #array_of_comb_numb: nx3 array, 1st colomn is the interacting atoms (comb_1_2), 2nd column is noe intensity, and 3rd column is 
     #return:  three arrays will be returned that have the first atom, second atom and the corresponding NOE intensit
     for i in range(len(array_of_comb_numb)):
-        if (array_of_comb_numb[i][2]>1.0):   # only atoms that are closer than 5.5 Angstroms will be considered
+        if (array_of_comb_numb[i][2]>1):   # only atoms that are closer than 5.5 Angstroms will be considered
             next
         else:
         
@@ -213,44 +216,52 @@ def atoms_distances(array_of_comb_numb):
             intensities.append(array_of_comb_numb[i][1]*conversion_factor)
             intensities.append(array_of_comb_numb[i][1]*conversion_factor)
     return small_dist_atoms_1, small_dist_atoms_2, intensities 
-x_H,y_H, intense=atoms_distances(variable_averagenoe)#x atom, y atom, and corresponding interaction
-XX_means=np.array(x_H)
-YY_means=np.array(y_H)
-intens=np.array(intense)
-x_atoms=np.array(shiftss)
-X_2d=[]
-Y_2d=[]
-
-X_points=np.arange(min(shiftss),max(shiftss),1)#gridpoints for axis
+x_H,y_H, intense=atoms_distances(variable_averagenoe) #x atom, y atom, and corresponding interaction
+XX_means=np.array(x_H)                                #atoms on x_axis
+YY_means=np.array(y_H)                                #atoms on y_axis
+intens=np.array(intense)                              #noe intensity
+x_atoms=np.array(shiftss)                             #shift of atoms on x_axis
+X_points=np.arange(min(shiftss),max(shiftss),1)       #gridpoints for axis
 N_atoms=len(X_points)
-for k in range(len(X_points)):
-    X_2d.append(X_points)
-    y=[]
-    for l in range(len(X_points)):
-        y.append(X_points[k])
-    Y_2d.append(y)
-X_2d=np.array(X_2d)
-Y_2d=np.array(Y_2d)
-Y_1d=np.arange(N_atoms)
 sigma=0.0001
 def gaus(x, y, intensity):
-    ZZZ=np.zeros((N_atoms,N_atoms))
-    for i in range(len(X_points)):
-        for k in range(len(X_points)):
-            for l in range(len(x)):
-                ZZZ[i][k]+=intensity[l]*math.exp(-((X_points[k]-x[l])**2+(X_points[i]-y[l])**2)/(2*sigma**2))
-                
+    ''' a function that takes in 3 arrays of atoms on x axis and atoms on y axis and the NOE intensity between corresponding atoms and returns a 2D array of gaussian corresponding to each point on the grid
+
+    Arguments:
+
+       x - an array of atom indeces that will be in x-axis of 2D NOESY spectrum
+       y - an array of atom indeces that will be in y-axis of 2D NOESY spectrum
+       intensity- an array of intensity for corresponding atom indeces
+    Returns:
+    
+       ZZZ-2D array that should have the gaussian for each interacting pair of atoms
+       
+    '''
+    ZZZ=np.zeros((len(X_points),len(X_points)))#N_atoms,N_atoms))                 #initialize 2d grid to zeros
+    for i in range(len(X_points)):                  #iterates through X atoms
+        for k in range(len(X_points)):              #iterates through Y atoms
+            for l in range(len(x)):                 #iterates though all neighbors that have to add to the guassian with their respective intensities  
+                ZZZ[i][k]+=intensity[l]*math.exp(-((x[i]-x[l])**2+(y[k]-y[l])**2)/(2*sigma**2))
     return ZZZ
-ZZ=gaus(XX_means, YY_means, intense)
-ZZ=np.array(ZZ)
-fig, ax = plt.subplots()
-len(shiftss), len(intens), len(x_H)
-sc=ax.scatter(x_H, y_H, intense)
+
+ZZ=gaus(XX_means, YY_means, intense)                  #adds gaussian to two interacting atoms with high NOE intensity
+ZZ=np.array(ZZ)                                       #converts to np array
+fig, ax = plt.subplots()                              #start plotting
+sc=ax.scatter(x_H, y_H,ZZ)# intense)                      #scatter plot of NOESY spectrum
 ax.grid()
 plt.xlabel('chemical shifts')
 plt.ylabel('chemical shifts')
-plt.title(r'Simulated 2D NOESY Spectroscopy of N atoms')
-plt.savefig('final_project.png'.format(colvar_name)) 
+plt.title(r'Scaller plot of 2D NOESY Spectroscopy of N atoms')
+plt.savefig('final_project_gaussian.png'.format(colvar_name)) 
+fig1, ax1 = plt.subplots()                            #start plotting
+sc=ax1.scatter(x_H, y_H,intense)                      #scatter plot of NOESY spectrum
+ax1.grid()
+plt.xlabel('chemical shifts')
+plt.ylabel('chemical shifts')
+plt.title(r'Gausian 2D NOESY Spectroscopy of N atoms')
+plt.savefig('final_project_scatter_plot.png'.format(colvar_name)) 
+plt.show()
+
 plt.show()
 
 
--- tests/test_final_project.py
+++ tests/test_final_project.py
@@ -12,7 +12,6 @@ Tests for `final_project` module.
 import sys
 import unittest
 from contextlib import contextmanager
-from click.testing import CliRunner
 
 from final_project import final_project
 from final_project import cli
@@ -21,20 +20,29 @@ from final_project import cli
 
 class TestFinal_project(unittest.TestCase):
 
-    def setUp(self):
+
+    def test_empty_arrays(self):
+        self.assertEqual(len(final_project.empty_arrays(3)), 3)
         pass
 
-    def tearDown(self):
+    def test_number_arrays(self):
+        self.assertNotEqual(len(final_project.noe_results),0)
         pass
-
-    def test_000_something(self):
+    def test_variables(self):
+        string=final_project.variables(final_project.colvar_file)
+        self.assertTrue(isinstance(string[0],str))
+        
+    def test_chemical_shifts(self):
+        self.assertTrue(isinstance(float(final_project.shiftss[3]),float))
         pass
+    def test_total_atom(self):
+        self.assertTrue(isinstance(final_project.new_variables[0],str))
+        self.assertEqual(len(final_project.variable_averagenoe[0]),3)
+        pass
+    def atoms_distances(self):
+        self.assertEqual(len(final_project.x_H),len(final_project.intense))
+        pass
+    def test_gaus(self):
+        self.assertEqual(len(final_project.ZZ[0]),final_project.N_atoms)
 
-    def test_command_line_interface(self):
-        runner = CliRunner()
-        result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert 'final_project.cli.main' in result.output
-        help_result = runner.invoke(cli.main, ['--help'])
-        assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
+        pass
